86f2c157ebbc2a4899aa2ff26bba50295324462f
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-14 15:17:39 -0400

    Making some methods that return name and weight

--- Item.java
+++ Item.java
@@ -7,7 +7,14 @@ public class Item
   private static String name;
 //The name of the item
 
-
+       //method that returns weight
+public String getWeight(){
+return weight;
+}
+       //method that returns name
+public String getName(){
+return name;
+}
 
 
 	public static void main(String[] args)
