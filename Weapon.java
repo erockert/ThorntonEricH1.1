fa13df850d4f62c8c6d1068e6b0b85758e71762f
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-14 16:01:12 -0400

    MY WEAPON CLASS COMPILED

---
+++ Weapon.java
@@ -0,0 +1,19 @@
+//new class for weapon
+public class Weapon extends Item
+{
+	//variable for damage
+	private int damage;
+
+	//method that returns damage
+public int getDamage()
+{
+return damage;
+}
+//This is my constructor for my weapon class
+public Weapon()
+{
+ super("Baseball bat", 6);
+}
+ public static void main(String[] args){
+}
+}
