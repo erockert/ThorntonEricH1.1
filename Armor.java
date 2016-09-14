4e08a838e1c1dc7174b717715934ce6e154aa8b8
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-14 19:24:50 -0400

    Making the armor method that returns the defense

--- Armor.java
+++ Armor.java
@@ -4,5 +4,11 @@ public class Armor extends Item
 //establishing variabes for the armor class
 private static int defense;
 
+//method that returns defense
+public int getDefense()
+{
+return defense;
+}
+
 }
 
