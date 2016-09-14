aecf080e6362c0752f0ac94f31eaf6ba841664a3
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-14 15:02:29 -0400

    When you dont feel like writing code so you wrote 2 variables in 2 hours

---
+++ Item.java
@@ -0,0 +1,18 @@
+public class Item
+  {
+//Properties:
+//weight, name
+  private static int weight;
+// The weight of the item
+  private static String name;
+//The name of the item
+
+
+
+
+	public static void main(String[] args)
+     {
+
+
+     }
+  }
