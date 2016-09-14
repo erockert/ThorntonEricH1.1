8537a56990f2d61811e4a67234fed112d9483f24
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-14 15:28:51 -0400

    doing some homework in class, making the weapon class now

--- Item.java
+++ Item.java
@@ -1,11 +1,12 @@
+//This is my item class thta Weapon, Armor and Nutrition inherit from
 public class Item
   {
-//Properties:
-//weight, name
+       //Properties:
+       //weight, name
   private static int weight;
-// The weight of the item
+       // The weight of the item
   private static String name;
-//The name of the item
+       //The name of the item
 
        //method that returns weight
 public String getWeight(){
@@ -16,6 +17,14 @@ public String getName(){
 return name;
 }
 
+       //new class for weapon
+public class Weapon extends Item
+{
+
+
+
+
+}
 
 	public static void main(String[] args)
      {
